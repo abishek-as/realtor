1fa842641bd0647bbc2002b35023bf07e62c36e5
abishek-as <abishek.codes@outlook.com>
2023-10-16 21:34:38 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import SignIn from "./pages/signIn";
+import SignUp from "./pages/signUp";
+import About from "./pages/about";
+import Profile from "./pages/profile";
+
 export default function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
