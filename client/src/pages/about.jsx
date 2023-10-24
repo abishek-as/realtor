7fd5dc2531b80f80684802678f3dbe1bd04c6561
abishek-as <abishek.codes@outlook.com>
2023-10-24 17:00:55 +0530

    add: home and about pages

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,3 +1,35 @@
 export default function About() {
-    return <div>About</div>;
+    return (
+        <div className="py-20 px-4 max-w-6xl mx-auto">
+            <h1 className="text-3xl font-bold mb-4 text-slate-800">
+                About RealTor
+            </h1>
+            <p className="mb-4 text-slate-700">
+                RealTor stands as a prominent real estate agency with a focus on
+                assisting clients in purchasing, selling, and leasing properties
+                in sought-after neighborhoods. Our team comprises seasoned
+                agents who are wholeheartedly committed to delivering top-tier
+                service and streamlining the property transaction experience for
+                our clients.
+            </p>
+            <p className="mb-4 text-slate-700">
+                At RealTor, our mission is to empower our clients to reach their
+                real estate objectives through the provision of expert guidance,
+                tailored assistance, and a profound grasp of the local real
+                estate landscape. Whether your aim is to purchase, sell, or
+                lease a property, we are your dedicated partners throughout your
+                entire journey.
+            </p>
+            <p className="mb-4 text-slate-700">
+                At RealTor, our agent team brings a rich reservoir of experience
+                and expertise in the real estate sector, and our unwavering
+                commitment lies in delivering the utmost quality of service to
+                our clients. We firmly hold the belief that the process of
+                buying or selling a property should be a thrilling and
+                gratifying journey, and we wholeheartedly dedicate ourselves to
+                transforming that belief into reality for every single one of
+                our clients.
+            </p>
+        </div>
+    );
 }
